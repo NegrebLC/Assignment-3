5d1db68ac29ffaf1f68cf22908e3161108c0b5ff
NegrebLC <BergCu2001@gmail.com>
2023-12-10 21:54:30 -0700

    j

--- docs/q1scripts.js
+++ docs/q1scripts.js
@@ -6,56 +6,49 @@ $(document).ready(function() {
         var targetSection = $(this).attr("href");
         $("section").hide();
         $(targetSection).show();
+
+        // If the target section is q2, fetch and display XML and JSON data
+        if (targetSection === "#q2") {
+            fetchXMLData();
+            fetchJSONData();
+        }
     });
 
     // Leaflet Map Initialization
-    var map = L.map('map').setView([52.246636, -113.8113], 13); // Set your initial coordinates and zoom level
+    var map = L.map('map').setView([52.246636, -113.8113], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     // Add a marker for your pet studio's location
-    var marker = L.marker([52.246636, -113.8113]).addTo(map); // Set your specific coordinates
+    var marker = L.marker([52.246636, -113.8113]).addTo(map);
 
     // Back to Blog Button Functionality
     $("#backButton").click(function() {
         $("section").hide();
         $("#blog").show();
     });
-});
 
-// Fetch and display data from XML file
-fetch('data.xml')
-.then(response => response.text())
-.then(data => {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(data, 'text/xml');
-    const people = xmlDoc.getElementsByTagName('person');
-    const list = document.getElementById('xmlPeopleList');
-
-    for (let person of people) {
-        const name = person.getElementsByTagName('name')[0].textContent;
-        const age = person.getElementsByTagName('age')[0].textContent;
-        const city = person.getElementsByTagName('city')[0].textContent;
-
-        const listItem = document.createElement('li');
-        listItem.textContent = `${name}, Age: ${age}, City: ${city}`;
-        list.appendChild(listItem);
+    // Function to fetch and display XML data
+    function fetchXMLData() {
+        fetch('data.xml')
+            .then(response => response.text())
+            .then(data => {
+                let xmlDataElement = $("#xmlData");
+                xmlDataElement.html("<h3>XML Data:</h3><pre>" + data + "</pre>");
+            })
+            .catch(error => console.log(error));
     }
-})
-.catch(error => console.error('Error fetching XML:', error));
 
-// Fetch and display data from JSON file
-fetch('data.json')
-.then(response => response.json())
-.then(data => {
-    const list = document.getElementById('jsonPeopleList');
-
-    data.forEach(person => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${person.name}, Age: ${person.age}, City: ${person.city}`;
-        list.appendChild(listItem);
-    });
-})
-.catch(error => console.error('Error fetching JSON:', error));
+    // Function to fetch and display JSON data
+    function fetchJSONData() {
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => {
+                let jsonDataElement = $("#jsonData");
+                jsonDataElement.html("<h3>JSON Data:</h3><pre>" + JSON.stringify(data, null, 2) + "</pre>");
+            })
+            .catch(error => console.log(error));
+    }
+});
